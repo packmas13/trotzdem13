65e1321a1970174fb3e4d6a56983839bb41f2f6e
oliverpool <git@olivier.pfad.fr>
2021-03-01 17:55:57 +0100

    team creation in frontend

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -14,7 +14,7 @@
     <!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap"> -->
 
     <!-- Styles -->
-    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <link rel="stylesheet" href="{{ mix('css/style.css') }}">
 
     <!-- ****** faviconit.com favicons ****** -->
     <link rel="shortcut icon" href="/favicon/favicon.ico">
