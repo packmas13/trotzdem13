fab804b8a0f9916c475fdc0c1451e93c29be4c51
oliverpool <git@olivier.pfad.fr>
2021-04-07 10:56:28 +0200

    public: team and challenge count on home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Challenge;
+use App\Models\Team;
 use Illuminate\Support\Carbon;
 
 class HomeController
@@ -10,6 +12,11 @@ class HomeController
     {
         $bannerStart = Carbon::createFromDate(2021, 4, 23, 'Europe/Berlin');
 
-        return view('home', ['days_left' => $bannerStart->diffInDays(), 'banner_start' => $bannerStart]);
+        return view('home', [
+            'days_left' => $bannerStart->diffInDays(),
+            'banner_start' => $bannerStart,
+            'team_count' => Team::whereNotNull('approved_at')->count(),
+            'challenge_count' => Challenge::published()->count(),
+        ]);
     }
 }
