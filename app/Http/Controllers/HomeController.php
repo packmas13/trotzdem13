6fb690154749b9fbb65a4882c97ed1abd1678074
oliverpool <git@olivier.pfad.fr>
2021-04-20 13:36:42 +0200

    public: livestream link

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,11 +10,22 @@ class HomeController
 {
     public function index()
     {
-        $bannerStart = Carbon::createFromDate(2021, 4, 23, 'Europe/Berlin');
+        $bannerStart = Carbon::create(2021, 4, 23, 18, 0, 0, 'Europe/Berlin');
+        $bannerEnd = Carbon::create(2021, 9, 18, 0, 0, 0, 'Europe/Berlin');
+
+        if($bannerStart->isFuture() || $bannerStart->isToday()){
+            $start_days_left = $bannerStart->diffInDays();
+        } else {
+            $start_days_left = -1;
+        }
 
         return view('home', [
-            'days_left' => $bannerStart->diffInDays(),
             'banner_start' => $bannerStart,
+            'start_days_left' => $start_days_left,
+
+            'banner_end' => $bannerEnd,
+            'end_days_left' => $bannerEnd->diffInDays(),
+
             'team_count' => Team::whereNotNull('approved_at')->count(),
             'challenge_count' => Challenge::published()->count(),
         ]);
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,22 @@
 <x-layout nav-main="/">
     <div class="mx-auto max-w-3xl sm:mt-2 flex flex-col justify-between text-white text-center font-black text-2xl leading-none">
-        <h2 class="bg-mango-600 px-1 py-3 mx-6 mt-3">Bannerlauf startet in
-            {{ $days_left }} Tagen, am {{ $banner_start->format('d.m.') }}
+        @if($start_days_left > 1)
+        <h2 class="bg-mango-600 px-1 py-3 mx-6 mt-3">
+            Bannerlauf startet in
+            {{ $start_days_left }} Tagen, am {{ $banner_start->format('d.m.') }}
         </h2>
+        @elseif(0 > $start_days_left)
+        <h2 class="bg-mango-600 px-1 py-3 mx-6 mt-3">
+            Die Banner laufen noch {{ $end_days_left }} Tage, bis zum {{ $banner_end->format('d.m.') }}
+        </h2>
+        @else
+        <h2 class="bg-mango-600 px-1 py-3 mx-6 mt-3">
+            Bannerlauf startet {{$start_days_left == 1?'morgen':'heute'}}, um 18 Uhr
+            <a href="https://youtu.be/ajqxr-0PEr8" target="_blank" rel="noopener noreferrer" class="bg-white text-mango-600 py-2 px-6 mt-3 inline-block font-bold">
+                Zum Gottesdienst-Livestream →
+            </a>
+        </h2>
+        @endif
         <video autoplay loop muted playsinline poster="/img/trotzdem_banner.jpg" width="768" heigh="432" tabindex="-1" aria-label="Wehendes DPSG Banner">
             <source src="/img/trotzdem_banner.webm" type="video/webm" />
             <source src="/img/trotzdem_banner.mp4" type="video/mp4" />
