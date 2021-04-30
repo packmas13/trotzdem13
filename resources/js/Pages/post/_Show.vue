6c96df40706a2c7039588d76400700b1b77057a9
Florian Kick <florian.kick@googlemail.com>
2021-04-30 17:38:01 +0200

    show reaction count and names

--- resources/js/Pages/post/_Show.vue
+++ resources/js/Pages/post/_Show.vue
@@ -43,7 +43,7 @@
             <path fill-rule="evenodd" d="M18 10c0 3.866-3.582 7-8 7a8.841 8.841 0 01-4.083-.98L2 17l1.338-3.123C2.493 12.767 2 11.434 2 10c0-3.866 3.582-7 8-7s8 3.134 8 7zM7 9H5v2h2V9zm8 0h-2v2h2V9zM9 9h2v2H9V9z" clip-rule="evenodd"></path>
         </svg>
         <span class="w-2" />
-        <Reactions class="flex-1" :reactedUsers="post.users" :post_id="post.id"></Reactions>
+        <Reactions class="flex-1" :reactions="post.reactions" :post_id="post.id"></Reactions>
     </div>
     <div class="mt-1 pl-10 border-gray-600">
         <Comments :comments="post.comments" />
