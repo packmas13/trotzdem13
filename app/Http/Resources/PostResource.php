2cda2eb23c74df9c6e46d1bbb19f807f53b87984
Florian Kick <florian.kick@googlemail.com>
2021-04-30 16:04:37 +0200

    bugfix: linked banner not displayed at post

--- app/Http/Resources/PostResource.php
+++ app/Http/Resources/PostResource.php
@@ -29,6 +29,7 @@ class PostResource extends JsonResource
 
             'team' =>  OtherTeam::make($this->whenLoaded('team')),
             'challenge' =>  Challenge::make($this->whenLoaded('challenge')),
+            'banner' => Banner::make($this->whenLoaded('banner')),
             'author' =>  OtherUser::make($this->whenLoaded('author')),
             'comments' => CommentResource::collection($this->whenLoaded('comments')),
             'users' => ReactionResource::collection($this->whenLoaded('users')),
