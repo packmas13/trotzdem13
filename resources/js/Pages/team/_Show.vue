f4a1989b88e64b1255c44fa0634c878da4d85ca0
Florian Kick <florian.kick@googlemail.com>
2021-05-06 23:10:32 +0200

    change team image

--- resources/js/Pages/team/_Show.vue
+++ resources/js/Pages/team/_Show.vue
@@ -10,7 +10,31 @@
     </div>
     <div class="md:flex mt-4">
         <div class="md:w-48 w-full flex-shrink-0 mr-2 text-center mb-2">
-            <img :src="team.image" class="inline" />
+            <img class="" :src="team.image || '/img/placeholder.png'" />
+            <div class="flex justify-start">
+                <svg
+                    v-if="team.image"
+                    @click="removeImage"
+                    class="w-6 h-6 cursor-pointer"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <title>Bild löschen</title>
+                    <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"></path>
+                </svg>
+                <svg
+                    @click="showChangeImage"
+                    class="w-6 h-6 cursor-pointer"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <title>Bild ändern</title>
+                    <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"></path>
+                </svg>
+            </div>
+            <change-team-image :team="team" v-if="changeImage" @finished="hideChangeImage"/>
         </div>
         <div class="flex-auto bg-gray-50 border rounded">
             <div v-if="!team.is_approved" class="p-5 bg-blue-100 text-blue-800">
@@ -165,9 +189,11 @@
 
 <script>
 import QRCode from "qrcode";
+import JetButton from "@/Jetstream/Button";
 import BannerPill from "../../components/BannerPill.vue";
 import ChallengeDetail from "../challenge/_Show";
 import OtherTeam from "./_OtherTeam.vue";
+import ChangeTeamImage from "@/components/ChangeTeamImage";
 
 export default {
     props: {
@@ -176,13 +202,16 @@ export default {
         },
     },
     components: {
+        ChangeTeamImage,
         BannerPill,
         ChallengeDetail,
         OtherTeam,
+        JetButton
     },
     data() {
         return {
             joinQrcode: null,
+            changeImage: false,
         };
     },
     computed: {
@@ -210,5 +239,20 @@ export default {
             this.joinQrcode = url;
         });
     },
+    methods: {
+        showChangeImage() {
+            this.changeImage = true;
+        },
+        hideChangeImage() {
+            this.changeImage = false;
+        },
+        removeImage() {
+            if (confirm("Bild wirklich löschen?")) {
+                this.$inertia.post(route("app.team.image", this.team.id), {
+                    onSuccess: () => this.cancelImage(),
+                });
+            }
+        }
+    }
 };
 </script>
---
+++ resources/js/components/ChangeTeamImage.vue
@@ -0,0 +1,109 @@
+<template>
+    <form @submit.prevent="changeImage">
+        <input
+            v-show="!imagePreview"
+            type="file"
+            accept="image/*"
+            class="mt-1 w-full rounded-md border-gray-300 focus:ring focus:ring-indigo-200"
+            ref="image"
+            @change="updatePreview"
+        />
+        <div v-if="imagePreview">
+            <div
+                class="block w-full h-48 my-1 bg-contain bg-no-repeat bg-center"
+                :style="
+                                        'background-image: url(\'' +
+                                        imagePreview +
+                                        '\');'
+                                    "
+            />
+            <label class="text-sm">
+                <input
+                    type="checkbox"
+                    required
+                    class="mr-1"
+                />Eine
+                <a
+                    class="text-link"
+                    target="_blank"
+                    href="/datenschutz/einwilligung-foto"
+                >schriftliche Vereinbarung</a
+                >
+                über die Online-Nutzung dieses Fotos für
+                die Aktion
+                <strong>Trotzdem13</strong>
+                liegt vor.
+            </label>
+            <div class="mt-1 flex justify-around">
+                <button
+                    :class="{ 'opacity-25': form.processing }"
+                    :disabled="form.processing"
+                    type="submit"
+                    class="secondary-button"
+                >
+                    <svg class="w-8 h-8" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <title>Speichern</title>
+                        <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path>
+                    </svg>
+                </button>
+                <button
+                    @click="cancelImage"
+                    type="button"
+                    class="cancel-button"
+                >
+                    <svg class="w-8 h-8" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <title>Abbrechen</title>
+                        <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+                    </svg>
+                </button>
+            </div>
+        </div>
+    </form>
+</template>
+
+<script>
+export default {
+    name: "ChangeTeamImage",
+    props: {
+        team: {
+            type: Object,
+        },
+    },
+    data() {
+        return {
+            imagePreview: null,
+            form: this.$inertia.form({
+                image: null,
+            }),
+        };
+    },
+    methods: {
+        changeImage() {
+            console.log('test')
+            this.form.image = null;
+            if (this.imagePreview) {
+                this.form.image = this.$refs.image.files[0];
+                this.form.post(route("app.team.image", this.team.id), {
+                    onSuccess: () => this.cancelImage(),
+                });
+            }
+        },
+        updatePreview() {
+            const reader = new FileReader();
+            reader.onload = (e) => {
+                this.imagePreview = e.target.result;
+            };
+            reader.readAsDataURL(this.$refs.image.files[0]);
+        },
+        cancelImage() {
+            this.$refs.image.value = null;
+            this.imagePreview = null;
+            this.$emit('finished');
+        },
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
