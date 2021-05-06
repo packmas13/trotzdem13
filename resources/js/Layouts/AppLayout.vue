231286afcc2347cd0cfb6718ad2c55ffcbbe0d9d
Florian Kick <florian.kick@googlemail.com>
2021-05-07 00:23:49 +0200

    added some icons

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -25,7 +25,12 @@
                                     :href="route('app.team.index')"
                                     :active="'app.team.index' == currentRoute"
                                 >
-                                    Gruppe
+                                    <div>
+                                        <svg class="w-8 h-8 m-auto" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z"></path>
+                                        </svg>
+                                        <span>Gruppe</span>
+                                    </div>
                                 </jet-nav-link>
 
                                 <jet-nav-link
@@ -33,7 +38,12 @@
                                     :active="'app.post.index' == currentRoute"
                                     v-if="$page.props.orga"
                                 >
-                                    Beiträge
+                                    <div>
+                                        <svg class="w-8 h-8 m-auto" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M2 5a2 2 0 012-2h7a2 2 0 012 2v4a2 2 0 01-2 2H9l-3 3v-3H4a2 2 0 01-2-2V5z"></path><path d="M15 7v2a4 4 0 01-4 4H9.828l-1.766 1.767c.28.149.599.233.938.233h2l3 3v-3h2a2 2 0 002-2V9a2 2 0 00-2-2h-1z"></path>
+                                        </svg>
+                                        <span>Beiträge</span>
+                                    </div>
                                 </jet-nav-link>
                             </div>
                         </div>
--- resources/js/Pages/team/Index.vue
+++ resources/js/Pages/team/Index.vue
@@ -28,7 +28,12 @@
                         <inertia-link
                             :href="route('app.team.join')"
                             class="secondary-button"
-                            >Gruppe beitreten</inertia-link
+                            >
+                            <svg class="w-10 h-8 m-auto" fill="currentColor" viewBox="0 0 30 20" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M13 6a3 3 0 11-6 0a3 3 0 016 0zM18 8a2 2 0 11-4 0a2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0a2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906M30.22 12.8a1.12 1.12 90 100-2.24H23.9637l1.4482-1.4482a1.12 1.12 90 10-1.5837-1.5837l-3.36 3.36a1.12 1.12 90 000 1.5837l3.36 3.36a1.12 1.12 90 001.5837-1.5837L23.9637 12.8H30.22z"></path>
+                            </svg>
+                            Gruppe beitreten
+                        </inertia-link
                         >
                         <inertia-link
                             :href="route('app.team.create')"
@@ -38,7 +43,12 @@
                                     ? 'secondary-button'
                                     : 'primary-button'
                             "
-                            >Neue Gruppe einrichten</inertia-link
+                            >
+                            <svg class="w-9 h-8 m-auto" fill="currentColor" viewBox="0 0 25 20" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M13 6a3 3 0 11-6 0a3 3 0 016 0zM18 8a2 2 0 11-4 0a2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0a2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906zM22 4a1 1 0 10-2 0v1h-1a1 1 0 100 2h1v1a1 1 0 102 0v-1h1a1 1 0 100-2h-1V4z"></path>
+                            </svg>
+                            Neue Gruppe einrichten
+                        </inertia-link
                         >
                     </div>
                 </div>
