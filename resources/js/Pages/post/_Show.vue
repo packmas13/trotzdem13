c1ef002a24efb654d78e0eb6877e021c134986eb
Florian Kick <florian.kick@googlemail.com>
2021-07-05 22:33:41 +0200

    Allow umlauts in hashtags
    Co-authored-by: oliverpool <3864879+oliverpool@users.noreply.github.com>

--- resources/js/Pages/post/_Show.vue
+++ resources/js/Pages/post/_Show.vue
@@ -113,7 +113,7 @@ export default {
             return this.route("app.post.index", {project: this.post.challenge_id});
         },
         contentWithHashtags() {
-            return this.post.content.replaceAll(/#([a-zA-Z0-9_]+)/g, (match) => {
+            return this.post.content.replaceAll(/#([a-zA-Z0-9_äöüÄÖÜß-]+)/g, (match) => {
                 let url = this.route("app.post.index", {hashtag: match});
                 return `<a href="${url}" class="text-link">${match}</a>`
             })
